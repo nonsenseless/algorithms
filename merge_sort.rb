1f0b3fa0c5476390cff729c99bbd7fa8a310522c
Sean <sean@nonsenseless.net>
2015-10-26 16:02:52 -0500

    Made some minor changes to refactor a portion of the code.

--- merge_sort.rb
+++ merge_sort.rb
@@ -14,26 +14,46 @@ Characteristics of Merge Sort
 Basic Strategy
 1) Split list by halves until you have single item lists
 2) For each pair of lists, compare the first item of each list and put the smallest first in a new merged list.
-=end
 
-def merge_sort(arr, result=[])
-  count = arr.count
-  if count == 1
-    return arr
-  else
-    a1 = merge_sort(arr[0..((count/2)-1)])
-    a2 = merge_sort(arr[(count/2)..-1])
+Comments on Implementation
+I factored out the merge portion into a separate function (merge_arrays) so now merge_sort recurses down to individual items which are then fed into the merge_arrays function. Sedgewick/Wayne noted in Algorithms, 4th edition, that MergeSort is basically an orderly way of managing the call to sort the subsets of the array. I think this implementation does that by cleanly separating the recursive splitting of the array from the sorting.
+
+I also considered factoring the initial recursion into a separate function so that the body of merge_sort would read:
+  def merge_sort(array, result=[])
+    a1, a2 = split_array(array)
+    result = merge_arrays(a1, a2)
   end
 
+but that would require adding in extra handling when the array was 1 item and, after accounting for that, the implementation didn't seem any cleaner or easier to understand than beforehand. Given that, I decided this was an appropriate instance to skip Metz's advice that methods be kept to 5 lines or fewer.
+=end
+
+def merge_sort(array)
+  count = array.count
+  mid = (count/2)-1
+
+  if count == 1
+    return array
+  else
+    a1 = merge_sort(array[0..mid])
+    a2 = merge_sort(array[mid+1..-1])
+  end
+
+  merge_arrays(a1, a2)
+end
+
+def merge_arrays(array1, array2)
+  result = []
+  count = array1.length + array2.length
+
   count.times do
-    if a1.empty?
-      result.push(a2.shift)
-    elsif a2.empty?
-      result.push(a1.shift)
-    elsif a1[0] < a2[0]
-      result.push(a1.shift)
+    if array1.empty?
+      result.push(array2.shift)
+    elsif array2.empty?
+      result.push(array1.shift)
+    elsif array1[0] < array2[0]
+      result.push(array1.shift)
     else
-      result.push(a2.shift)
+      result.push(array2.shift)
     end
   end
 
